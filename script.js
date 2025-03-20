9b1f176ea9be9f75d7606fdd4d6bc5361e1ef81c
avi-711 <uchiha07112000@gmail.com>
2025-03-21 00:39:44 +0530

    hello avi

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 let plus1 = 10 ;
 let plus2 = 20 ;
 console.log(plus1+plus2)
+
+let minus1 = 10;
+let minus2 = 20;
+console.log(minus1-minus2)
+
+let divide1 = 10;
+let divide2 = 20;
+console.log(divide1/divide2)
+
+let modules1 = 10;
+let modules2 = 20;
+console.log(modules1%modules2)
+
+let multiple1 = 10;
+let multiple2 = 20;
+console.log(multiple1*multiple2)
+
+let redf = 10;
+redf += 10;
+console.log(redf)
+
+
+let age1 = 19;
+{
+    if(age1>19)
+        console.log("i love you")
+    else
+    console.log("i like you")
+}
+
+
+for (let num = 1 ; num < 10; num++)
+    console.log(num)
+
+for (let revnum =10 ;
+    revnum > 1; revnum--)
+    console.log(revnum)
+
