009934121fef4b8e049f30f50e4d4f2958edb74c
avi-711 <uchiha07112000@gmail.com>
2025-04-16 07:15:49 +0530

    hi avi

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+/*
+
+
 // arithmetic Operators 
 
 // addition operators 
@@ -141,13 +145,223 @@ let stri4 = "avi";
 
 console.log(stri3 === stri4);
 
+// and && 
+
+let and1 = true;
+let and2 = false;
+let and3 = true;
+
+console.log( and1 && and2 );   // false
+console.log(and2 && and3 );   // false 
+console.log(and3 && and1 );  // true 
+
+let and4 = 0 ;
+let and5 = 1 ;
+let and6 = 1 ;
+
+console.log(and4 && and5 );  // false 
+console.log(and5 && and6 );  // true 
+console.log(and6 && and4 );   // false 
+
+// or || 
+
+let or1 = true ;
+let or2 = false ;
+let or3 = false ;
+
+console.log(or1 || or2 ); // true 
+console.log(or2 || or3 ); // false 
+console.log(or3 || or1 ); //  true 
+
+
+ // not ! 
+
+ let alert1 = false ;
+ let alert2 ;
+ let alert3 = NaN ;
+ let mySum = 45;
+ let gone = false ;
+ let isFirst = true ;
+
+ console.log(!alert1);
+ console.log(alert2);
+ console.log(alert3);
+ console.log(mySum);
+ console.log(gone);
+ console.log(isFirst);
+
+//  ternary operators 
+
+let myage = 18 ;
+
+(myage >= 18 ) ? (console.log("i like you ") ) : (console.log("i love ..."));
+
+
+
+// conditionals   
+
+let drive = 19;
+if(drive >= 18)
+{
+    console.log("can drive")
+
+}
+
+    else {
+console.log("connot drive")
+    }
+ 
+
+    let number = 5;
+    if(number == 1)
+      {
+        console.log("A")
+      }
+
+      else if (number == 2)
+      {
+        console.log("B")
+      }
+
+      else if (number == 3)
+      {
+        console.log ("c")
+      }
+ 
+      else if (number == 4)
+
+        {
+            console.log("D")
+        }
+
+        else if ( number == 5)
+        {
+            console.log("f")
+        }
+
+        else {
+            console.log("E");
+        };
 
 
 
 
+        let speed = 39;
+        if(speed > 50 )
+{
+  console.log("over speed ");
+}
+ 
+if(speed > 30 && speed <= 40)
+{
+  console.log( "normal speed ");
+}
+if (speed < 30 )
+{
+  console.log("slow");
+}
+
+let myage1 = 18   ;
+if (myage1 >= 18 )
+{
+  console.log("i can drive");
+}
+
+else {
+  console.log("i connot drive ");
+}
+
+
+let apple = 3 ;
+switch(apple) {
+  case "or":
+    console.log("hello");
+    break;
+    case - 3 :
+      console.log("hi avi ");
+
+      break;
+      default:
+      console.log("hiaa");
+
+}
+
+// for  loop 
+
+for (let i = 1 ; i <= 5 ; i++);
+{
+  console.log("avi ")
+}
+
+
+for (let avi = 2 ; avi <= 5 ; avi++);
+{
+  console.log(avi)
+}
 
 
 
+// break 
 
+for( let x = 1 ; x <= 10 ; x++ )
+  if (x == 4)
+  {
+    break;
+  }
+else {
+  console.log(x);
+}
+
+// continue 
+
+for ( let i = 1 ; i <= 7; i++)
+if (i == 5)
+{
+  continue;
+}
+else {
+  console.log(i);
+}
+
+
+// while loop 
+
+ let a = 1 ;
+ while(a <= 5)
+
+ {
+  if(a == 3 )
+   
+  {
+    a++;
+    continue;
+  }
+  else {
+    console.log(a);
+  }
+  a++;
+ }
+
+
+ let i = 1 ;
+ while (i <= 8)
+ {
+  if (i == 5)
+  {break}
+  else {
+    console.log(i);
+    i++;
+  }
+ }
+
+//  do while 
+
+let age =  1 ;
+do {
+  console.log("hello");
+  age++;
+}while(age <= 10);  
+
+*/
 
 
